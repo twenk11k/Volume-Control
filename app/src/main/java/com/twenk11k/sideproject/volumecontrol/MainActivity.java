856f902298cea763a4c7942d7d126ad9d9382a06
twenk11k <selimdemiral@hotmail.com>
2018-07-27 23:05:43 +0300

    Add seekbar

--- app/src/main/java/com/twenk11k/sideproject/volumecontrol/MainActivity.java
+++ app/src/main/java/com/twenk11k/sideproject/volumecontrol/MainActivity.java
@@ -1,13 +1,55 @@
 package com.twenk11k.sideproject.volumecontrol;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SeekBar seekBarVolume = null;
+    private AudioManager audioManager = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        setSeekBar();
     }
+    private void setSeekBar() {
+        try
+        {
+            seekBarVolume = (SeekBar)findViewById(R.id.seekbar);
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            seekBarVolume.setMax(audioManager
+                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+            seekBarVolume.setProgress(audioManager
+                    .getStreamVolume(AudioManager.STREAM_MUSIC));
+            seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+            {
+                @Override
+                public void onStopTrackingTouch(SeekBar arg0)
+                {
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar arg0)
+                {
+                }
+
+                @Override
+                public void onProgressChanged(SeekBar arg0, int progress, boolean arg2)
+                {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                            progress, 0);
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
