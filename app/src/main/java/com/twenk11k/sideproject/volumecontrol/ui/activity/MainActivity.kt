1a687f07a8b9c6841b68cf784115b20a8c3d5345
Selim Demiral <selim_demiral@outlook.com>
2022-07-31 21:41:45 +0300

    Use view binding instead of data binding in MainActivity

--- app/src/main/java/com/twenk11k/sideproject/volumecontrol/ui/activity/MainActivity.kt
+++ app/src/main/java/com/twenk11k/sideproject/volumecontrol/ui/activity/MainActivity.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import android.media.AudioManager
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.twenk11k.sideproject.volumecontrol.R
 import com.twenk11k.sideproject.volumecontrol.audiovolume.AudioVolumeObserver
@@ -13,11 +13,7 @@ import com.twenk11k.sideproject.volumecontrol.audiovolume.OnAudioVolumeChangedLi
 import com.twenk11k.sideproject.volumecontrol.databinding.ActivityMainBinding
 import com.twenk11k.sideproject.volumecontrol.pref.ProgressPrefs
 
-class MainActivity : DataBindingActivity(), OnAudioVolumeChangedListener {
-
-    // views
-    private lateinit var seekBar: SeekBar
-    private lateinit var imgMusicNote: ImageView
+class MainActivity : AppCompatActivity(), OnAudioVolumeChangedListener {
 
     private var audioManager: AudioManager? = null
     private var sBarProgress = 0
@@ -26,10 +22,12 @@ class MainActivity : DataBindingActivity(), OnAudioVolumeChangedListener {
     private var musicOffDrawable: Drawable? = null
     private val preferences = ProgressPrefs()
 
-    private val binding: ActivityMainBinding by binding(R.layout.activity_main)
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         volumeControlStream = AudioManager.STREAM_MUSIC
         setViews()
     }
@@ -38,7 +36,7 @@ class MainActivity : DataBindingActivity(), OnAudioVolumeChangedListener {
         super.onResume()
         if (audioManager != null) {
             val currentVol = audioManager!!.getStreamVolume(AudioManager.STREAM_MUSIC)
-            seekBar.progress = currentVol
+            binding.seekBar.progress = currentVol
             handleIcon(currentVol)
         }
         if (audioVolumeObserver == null) {
@@ -53,8 +51,6 @@ class MainActivity : DataBindingActivity(), OnAudioVolumeChangedListener {
     }
 
     private fun setViews() {
-        seekBar = binding.seekBar
-        imgMusicNote = binding.imgMusicNote
         setSeekBar()
         setDrawables()
         setViewListeners()
@@ -66,29 +62,37 @@ class MainActivity : DataBindingActivity(), OnAudioVolumeChangedListener {
     }
 
     private fun setViewListeners() {
-        imgMusicNote.setOnClickListener {
-            if (imgMusicNote.drawable == musicNoteDrawable) {
+        binding.imgMusicNote.setOnClickListener {
+            if (binding.imgMusicNote.drawable == musicNoteDrawable) {
                 preferences.seekBarProgress = sBarProgress
-                seekBar.progress = 0
+                binding.seekBar.progress = 0
                 sBarProgress = 0
-                audioManager?.setStreamVolume(AudioManager.STREAM_MUSIC, sBarProgress, AudioManager.FLAG_PLAY_SOUND)
-                imgMusicNote.setImageDrawable(musicOffDrawable)
+                audioManager?.setStreamVolume(
+                    AudioManager.STREAM_MUSIC,
+                    sBarProgress,
+                    AudioManager.FLAG_PLAY_SOUND
+                )
+                binding.imgMusicNote.setImageDrawable(musicOffDrawable)
             } else {
                 sBarProgress = preferences.seekBarProgress
-                audioManager?.setStreamVolume(AudioManager.STREAM_MUSIC, sBarProgress, AudioManager.FLAG_PLAY_SOUND)
-                seekBar.progress = sBarProgress
-                imgMusicNote.setImageDrawable(musicNoteDrawable)
+                audioManager?.setStreamVolume(
+                    AudioManager.STREAM_MUSIC,
+                    sBarProgress,
+                    AudioManager.FLAG_PLAY_SOUND
+                )
+                binding.seekBar.progress = sBarProgress
+                binding.imgMusicNote.setImageDrawable(musicNoteDrawable)
             }
         }
     }
 
     private fun setSeekBar() {
-        seekBar.secondaryProgress = 100
+        binding.seekBar.secondaryProgress = 100
         audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        seekBar.max = audioManager!!.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        binding.seekBar.max = audioManager!!.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
         val volume = audioManager!!.getStreamVolume(AudioManager.STREAM_MUSIC)
-        seekBar.progress = volume
-        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        binding.seekBar.progress = volume
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 sBarProgress = progress
                 handleIcon(progress)
@@ -97,22 +101,25 @@ class MainActivity : DataBindingActivity(), OnAudioVolumeChangedListener {
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                audioManager?.setStreamVolume(AudioManager.STREAM_MUSIC, sBarProgress, AudioManager.FLAG_PLAY_SOUND)
+                audioManager?.setStreamVolume(
+                    AudioManager.STREAM_MUSIC,
+                    sBarProgress,
+                    AudioManager.FLAG_PLAY_SOUND
+                )
             }
         })
     }
 
     private fun handleIcon(currentVol: Int) {
         if (currentVol == 0) {
-            imgMusicNote.setImageDrawable(musicOffDrawable)
+            binding.imgMusicNote.setImageDrawable(musicOffDrawable)
         } else {
-            imgMusicNote.setImageDrawable(musicNoteDrawable)
+            binding.imgMusicNote.setImageDrawable(musicNoteDrawable)
         }
     }
 
     override fun onAudioVolumeChanged(currentVolume: Int, maxVolume: Int) {
-        seekBar.progress = currentVolume
+        binding.seekBar.progress = currentVolume
         handleIcon(currentVolume)
     }
-
 }
